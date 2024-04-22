ca210c60ea2feed492be35248fcfea41ed6574bf
John Bucaria <john@bucaria.co>
2024-04-22 09:55:11 -0400

    new weather app

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
+import axios from 'axios'
+import queryString from 'query-string'
+
 const locationInput = document.getElementById('locationInput')
 const weatherInfo = document.getElementById('weatherInfo')
+const buttonEl = document.getElementById('submit')
 
-document.getElementById('submit').addEventListener('click', () => {
-  const location = locationInput.value
-  const encodedString = encodeURIComponent(location)
+buttonEl.addEventListener('click', () => {
+  const getTimelineURL = 'https://api.tomorrow.io/v4/timelines'
+  const apikey = 'eJly8pd7iRjR0Qks5O9pgQ1bwEw0ymXp'
+  const locale = locationInput.value
+  const encodedString = encodeURIComponent(locale)
+  const location = []
 
-  axios
-    .get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedString}.json?proximity=ip&access_token=pk.eyJ1IjoiamJ1Y2FyaWEiLCJhIjoiY2x2OGY1N2NrMGp0bjJqcHFobmE4M3QzaCJ9.jHh_g9nCSNFZL5lAIUErQA&limit=1`)
-    .then(response => {
-      const apiResponse = response.data
-      const latitude = apiResponse.features[0].center[1]
-      const longitude = apiResponse.features[0].center[0]
+  axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedString}.json?proximity=ip&access_token=pk.eyJ1IjoiamJ1Y2FyaWEiLCJhIjoiY2x2OGY1N2NrMGp0bjJqcHFobmE4M3QzaCJ9.jHh_g9nCSNFZL5lAIUErQA&limit=1`).then(response => {
+    const apiResponse = response.data
+    const latitude = apiResponse.features[0].center[1]
+    const longitude = apiResponse.features[0].center[0]
 
-      console.log(latitude, longitude)
+    let location = [`${longitude}, ${latitude}`]
 
-      const params = {
-        access_key: 'f4d4cd63ee90a5440e491fef5f10fd11',
-        query: `${latitude},${longitude}`,
-        units: 'f',
-      }
-      return axios.get('http://api.weatherstack.com/current', { params })
-    })
-    .then(response => {
-      const apiResponse = response.data
-      weatherInfo.innerText = `Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}ºF and wind speed is ${apiResponse.current.wind_speed}mph`
-    })
-    .catch(error => {
-      console.log('Can not connect to weather app')
-    })
+    const fields = ['precipitationIntensity', 'precipitationType', 'windSpeed', 'windGust', 'windDirection', 'temperature', 'temperatureApparent', 'cloudCover', 'cloudBase', 'cloudCeiling', 'weatherCode']
+
+    const units = 'imperial'
+
+    const now = new Date()
+
+    const timezone = 'America/New_York'
+
+    const getTimelineParameters = queryString.stringify(
+      {
+        apikey,
+        location,
+        fields,
+        now,
+        units,
+        timezone,
+      },
+      { arrayFormat: 'comma' }
+    )
+    return axios
+      .get(getTimelineURL + '?' + getTimelineParameters)
+      .then(response => {
+        const timelines = response.data.data.timelines
+        const firstTimeline = timelines[0]
+        const firstInterval = firstTimeline.intervals[0]
+        const temperature = firstInterval.values.temperature
+        const wind = firstInterval.values.windSpeed
+        weatherInfo.innerText = `Tempature in ${locale} is ${temperature}°F and Wind Speed is ${wind}mph`
+      })
+      .catch(error => {
+        console.log('Error', error)
+      })
+  })
 })
